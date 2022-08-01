454b64027515af69d36fb90fba673d9555556690
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-08-01 23:43:13 +0530

    Start the Notification app

---
+++ schedulers/emailScheduler.js
@@ -0,0 +1,38 @@
+const cron = require('node-cron');
+const Notification = require('../models/notification.model');
+const emailTransporter = require('../notifiers/emailService');
+const constants = require('../utils/constants');
+
+cron.schedule("*/5 * * * * *", async () => {
+
+
+    console.log("inside the shceduler");
+   
+    const notifications = await Notification.find({ status : constants.status.unsend });
+    console.log("================================");
+    console.log(notifications);
+
+    if (notifications) {
+        console.log("Number of un-sent requests are : ", notifications.length);
+
+        notifications.forEach(n => {
+            
+            const mailObj = {
+                to: n.recepientEmails,
+                subject: n.subject,
+                text: n.content,
+            };
+            console.log("Sending email for ", mailObj);
+            emailTransporter.sendMail(mailObj, async (err, info) => {
+                if (err) {
+                    console.log("Error while sending email ", err.message);
+                } else {
+                    console.log("Successfully sent the email :", info);
+
+                    n.status = constants.status.send;
+                    await n.save();
+                }
+            });
+        });
+    }
+});
--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose")
+const dbConfig = require('./configs/db.config');
 const serverConfig = require('./configs/server.config');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+db.on("error", () => {
+    console.log("Error while connecting to MongoDB");
+});
+db.once("open", () => {
+    console.log("Connected to mongoDB");
+});
 
 
+require('./routes/notification.route')(app);
 
+//attach the cron file 
+require('./schedulers/emailScheduler');
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`Server Started At ${serverConfig.PORT}`);
-})
+});
